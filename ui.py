87950174e45e1a192ad62b84d3a477e5ffd547a3
Atkuri Bala Naga Sai Narasimha Subramanya Rishik <rishikcr72401@gmail.com>
2026-05-31 18:59:59 +0530

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,90 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+THEME_COLOR = "#375362"
+FONT = ("Courier",20,"normal")
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain ):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20,pady=20,bg = THEME_COLOR)
+
+        self.score_label = Label(
+            text="Score: 0",
+            bg= THEME_COLOR,
+            highlightthickness=0,
+            fg = 'white',
+            font= ("Arial",15,'normal')
+        )
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(
+            width=300,
+            height=250,
+            bg = "white",
+            highlightthickness=0)
+        self.canvas.grid(row=1,column=0,columnspan=2,padx=30,pady=30)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width = 280, # it wraps the sentence into the box less than canva width
+            text = "Question",
+            font = FONT,
+            fill=THEME_COLOR)
+
+        true_img = PhotoImage(file = "./images/true.png")
+        self.true_button = Button(
+            image=true_img,
+            bg= THEME_COLOR,
+            highlightthickness=0,
+            command= self.true_pressed
+        )
+        self.true_button.grid(row = 2, column=0)
+
+        false_img = PhotoImage(file = "./images/false.png")
+        self.false_button = Button(
+            image= false_img,
+            bg= THEME_COLOR,
+            highlightthickness=0,
+            command= self.false_pressed
+        )
+        self.false_button.grid(row = 2,column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg='white')
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text = q_text)
+
+            self.score_label.config(text = f"Score: {self.quiz.score}")
+        else:
+            self.canvas.itemconfig(self.question_text,text = f"Quiz is completed. Your Score: {self.quiz.score}/10")
+            self.true_button.config(state = "disabled")
+            self.false_button.config(state="disabled")
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer('False'))
+
+    def give_feedback(self,is_right):
+
+        if is_right:
+            self.canvas.config(bg = 'lime green')
+        else:
+            self.canvas.config(bg = 'red')
+
+        self.window.after(1000, self.get_next_question)
+
+
+
+
+
+
+
+
+
